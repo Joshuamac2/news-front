f4456b667d0d37a216d8bec60861a1033ffb1e2a
Joshua <joshuamacleod@live.com>
2021-03-15 17:42:14 +0000

    Redux implemented for state management

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Provider } from "react-redux";
+import store from "../store";
+
 import { NavBar, Landing } from '../components';
 import { ArticlesList, ArticlesInsert } from '../pages';
 import Register from "../components/auth/Register.js";
@@ -10,16 +13,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-      <Route path="/" exact component={Landing} />
-      <Route path="/register" exact component={Register} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/articles/list" exact component={ArticlesList} />
-      <Route path="/articles/create" exact component={ArticlesInsert} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <NavBar />
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/articles/list" exact component={ArticlesList} />
+            <Route path="/articles/create" exact component={ArticlesInsert} />
+          </Switch>
+      </Router>
+    </Provider>
   )
 }
 
