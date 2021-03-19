39b8df1021229c249897a0d47e4c85d2e6a8a52b
Joshua <joshuamacleod@live.com>
2021-03-19 21:07:17 +0000

    bbc api key

---
+++ src/NewsContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=bbc&from=2021-02-19&sortBy=publishedAt&apiKey=${API_KEY}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
